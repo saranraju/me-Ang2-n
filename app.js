b9e7960b3844bfc9f60bd7f306e8809ddf3d7921
logesh4cspl <logeswaran.n@cspl.com>
2016-11-06 01:44:05 +0530

    removed es5 syntax

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-/*var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var app = express();*/
-
 "use strict";
 import express      from 'express';
 import path         from 'path';
