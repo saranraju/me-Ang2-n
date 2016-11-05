daeab1e5b65811b9290d377429fa379a7e98153a
logesh4cspl <logeswaran.n@cspl.com>
2016-11-06 00:07:06 +0530

    router configurations

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -22,8 +19,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+/**
+ * Routes
+ */
+var route_arr = ['index'];
+if (app.get('env')=='dev') {
+  // try new things, keep production lighter
+  route_arr.push('_mock');
+}
+for (i in route_arr) {
+  app.use('/'+route_arr[i],require('./routes/'+route_arr[i]));
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,7 +42,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'dev' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
